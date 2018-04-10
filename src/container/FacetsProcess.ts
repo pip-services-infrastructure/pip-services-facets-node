1d62cd1b468c1b72c2cd29eddf858947ebdeb4f8
Dmitriy Krayniy <krdima92@gmail.com>
2018-04-10 15:47:52 +0300

    added pip-services-oss

--- src/container/FacetsProcess.ts
+++ src/container/FacetsProcess.ts
@@ -1,5 +1,6 @@
 import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
+import { DefaultOssFactory } from 'pip-services-oss-node';
 
 import { FacetsServiceFactory } from '../build/FacetsServiceFactory';
 
@@ -8,6 +9,7 @@ export class FacetsProcess extends ProcessContainer {
     public constructor() {
         super("facets", "Faceted search microservice");
         this._factories.add(new FacetsServiceFactory);
+        this._factories.add(new DefaultOssFactory);
     }
 
 }
