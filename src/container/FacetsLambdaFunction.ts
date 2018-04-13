78346d7ea3bc06f8c42960666fc4796fb2279010
Dmitriy Krayniy <krdima92@gmail.com>
2018-04-13 18:40:03 +0300

    added elasticsearch,prometheus,heartbeat

--- src/container/FacetsLambdaFunction.ts
+++ src/container/FacetsLambdaFunction.ts
@@ -1,5 +1,8 @@
 import { Descriptor } from 'pip-services-commons-node';
 import { CommandableLambdaFunction } from 'pip-services-aws-node';
+import { DefaultNetFactory } from 'pip-services-net-node';
+import { DefaultOssFactory } from 'pip-services-oss-node';
+
 import { FacetsServiceFactory } from '../build/FacetsServiceFactory';
 
 export class FacetsLambdaFunction extends CommandableLambdaFunction {
@@ -7,6 +10,8 @@ export class FacetsLambdaFunction extends CommandableLambdaFunction {
         super("facets", "Faceted search function");
         this._dependencyResolver.put('controller', new Descriptor('pip-services-facets', 'controller', 'default', '*', '*'));
         this._factories.add(new FacetsServiceFactory());
+        this._factories.add(new DefaultNetFactory);
+        this._factories.add(new DefaultOssFactory);
     }
 }
 
